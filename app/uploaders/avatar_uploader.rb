a1fb18ca42f598d8749492052a8aab7ce3d94685
9Jimmy <9jimmyw@gmail.com>
2019-07-15 09:53:12 +0500

    resized image in show

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -13,6 +13,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  version :resized do 
+     process :resize_to_fit => [700, 10000]
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
